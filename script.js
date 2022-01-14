36e2bead10bf8f98442de570acc7f6d79595447f
stantoha <78742661+stantoha@users.noreply.github.com>
2022-01-14 23:09:27 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,51 @@
+    //Modal
 
-const menu=document.querySelector('.menu');
-const nav=document.querySelector('.nav');
+    const modal = document.querySelector('.modal'),
+        modalTrigger = document.querySelectorAll('[data-modal]'),
+        modalClosebtn = document.querySelector('[data-close]');
+
+    function openModal() {
+        modal.classList.add('show'); //или modal.classList.toggle('show);
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', openModal);
+    });
+
+    function closeModal() {
+        modal.classList.remove('show'); //или modal.classList.toggle('show);
+        modal.classList.add('hide');
+        document.body.style.overflow = '';
+    }
+
+    modalClosebtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === "Escape" && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+
+      const modalTimerId=setTimeout(openModal,5000);
+
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
+
+
 
